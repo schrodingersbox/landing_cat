5ab9207beb6cc1e769e4b5007b11f7bda7362e19
Rich Humphrey <rdh727@gmail.com>
2014-01-16 01:24:44 -0800

    Updated engine config to use rspec generators

--- lib/landing_cat/engine.rb
+++ lib/landing_cat/engine.rb
@@ -1,5 +1,14 @@
 module LandingCat
   class Engine < ::Rails::Engine
     isolate_namespace LandingCat
+
+    # http://viget.com/extend/rails-engine-testing-with-rspec-capybara-and-factorygirl
+
+    config.generators do |g|
+         g.test_framework      :rspec,        :fixture => false
+         g.fixture_replacement :factory_girl, :dir => 'spec/factories'
+         g.assets false
+         g.helper false
+    end
   end
 end
